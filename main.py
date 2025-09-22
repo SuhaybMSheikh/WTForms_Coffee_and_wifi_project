c0dc6f0454ef332d94dd9c4f36a81696df6bdef4
Viiperlol <suhaybsheikh@hotmail.com>
2025-09-22 21:45:16 +0800

    commit

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class CafeForm(FlaskForm):
 
 
 
-# make coffee/wifi/power a select element with choice of 0 to 5.
+
 #e.g. You could use emojis ☕️/💪/✘/🔌
 # make all fields required except submit
 # use a validator to check that the URL field has a URL entered.
